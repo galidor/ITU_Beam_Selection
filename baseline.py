419a7444ea34a6c5d5095e02315ff277699c5834
galidor <galidor94@gmail.com>
2020-08-18 13:33:57 +0200

    initial

---
+++ baseline.py
@@ -0,0 +1,103 @@
+from models import LidarMarcus
+from torch import nn
+from torchvision import transforms
+import numpy as np
+import torch
+from torch.utils.data import TensorDataset, DataLoader
+import tqdm
+from torch import optim
+from ignite.metrics import TopKCategoricalAccuracy
+import torch.nn.functional as F
+
+
+def beams_log_scale(y, thresholdBelowMax):
+    y_shape = y.shape
+
+    for i in range(0, y_shape[0]):
+        thisOutputs = y[i, :]
+        logOut = 20 * np.log10(thisOutputs + 1e-30)
+        minValue = np.amax(logOut) - thresholdBelowMax
+        zeroedValueIndices = logOut < minValue
+        thisOutputs[zeroedValueIndices] = 0
+        thisOutputs = thisOutputs / sum(thisOutputs)
+        y[i, :] = thisOutputs
+
+    return y
+
+def get_beam_output(output_file):
+    thresholdBelowMax = 6
+
+    print("Reading dataset...", output_file)
+    output_cache_file = np.load(output_file)
+    yMatrix = output_cache_file['output_classification']
+
+    yMatrix = np.abs(yMatrix)
+    yMatrix /= np.max(yMatrix)
+    yMatrixShape = yMatrix.shape
+    num_classes = yMatrix.shape[1] * yMatrix.shape[2]
+
+    y = yMatrix.reshape(yMatrix.shape[0], num_classes)
+    y = beams_log_scale(y, thresholdBelowMax)
+
+    return y, num_classes
+
+
+def evaluate(net, test_dataloader):
+    with torch.no_grad():
+        net.eval()
+        top_10 = TopKCategoricalAccuracy(k=10)
+        for i, data in enumerate(test_dataloader):
+            lidar, beams = data
+            lidar = lidar.cuda()
+            beams = beams.cuda()
+            preds = net(lidar)
+            preds = F.softmax(preds, dim=1)
+            top_10.update((preds, beams))
+        net.train()
+        print(top_10.compute())
+
+
+
+if __name__ == '__main__':
+    lidar_data_train = np.load("/home/galidor/Documents/ITU_Beam_Selection/data/baseline_data/lidar_input/lidar_train.npz")['input']
+    lidar_data_test = np.load("/home/galidor/Documents/ITU_Beam_Selection/data/baseline_data/lidar_input/lidar_validation.npz")['input']
+
+    coord_data_train = np.load("/home/galidor/Documents/ITU_Beam_Selection/data/baseline_data/coord_input/coord_train.npz")['coordinates']
+    coord_data_test = np.load("/home/galidor/Documents/ITU_Beam_Selection/data/baseline_data/coord_input/coord_validation.npz")['coordinates']
+
+    beam_data_train = np.load("/home/galidor/Documents/ITU_Beam_Selection/data/baseline_data/beam_output/beams_output_train.npz")['output_classification']
+    beam_data_test = np.load("/home/galidor/Documents/ITU_Beam_Selection/data/baseline_data/beam_output/beams_output_validation.npz")['output_classification']
+
+    beam_output_train = get_beam_output("/home/galidor/Documents/ITU_Beam_Selection/data/baseline_data/beam_output/beams_output_train.npz")
+    beam_output_test = get_beam_output("/home/galidor/Documents/ITU_Beam_Selection/data/baseline_data/beam_output/beams_output_validation.npz")
+
+    lidar_data_train = torch.from_numpy(lidar_data_train).float()
+    lidar_data_test = torch.from_numpy(lidar_data_test).float()
+
+    beam_output_train = torch.from_numpy(beam_output_train[0]).float()
+    beam_output_test = torch.from_numpy(beam_output_test[0]).float()
+
+    train_dataset = TensorDataset(lidar_data_train, beam_output_train)
+    train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+
+    test_dataset = TensorDataset(lidar_data_test, beam_output_test)
+    test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=False)
+
+    model = LidarMarcus().cuda()
+    optimizer = optim.Adam(model.parameters())
+    criterion = torch.nn.BCEWithLogitsLoss(reduction='mean').cuda()
+    tbar = tqdm.tqdm(enumerate(train_dataloader), total=len(train_dataloader))
+
+    evaluate(model, test_dataloader)
+
+    for i, data in tbar:
+        optimizer.zero_grad()
+        lidar, beams = data
+        lidar = lidar.cuda()
+        beams = beams.cuda()
+        preds = model(lidar)
+        loss = criterion(preds, beams)
+        loss.backward()
+        optimizer.step()
+        tbar.set_postfix_str(str(loss.item()))
+    evaluate(model, test_dataloader)
---
+++ models.py
@@ -0,0 +1,33 @@
+from torch import nn
+from torch.nn import functional as F
+
+class Flatten(nn.Module):
+    def forward(self, input):
+        return input.view(input.size(0), -1)
+
+class LidarMarcus(nn.Module):
+    def __init__(self):
+        super(LidarMarcus, self).__init__()
+        self.layers = nn.Sequential(nn.Conv2d(20, 10, kernel_size=13, stride=1, padding=6),
+                                    nn.ReLU(),
+                                    nn.Conv2d(10, 30, kernel_size=11, stride=1, padding=5),
+                                    nn.ReLU(),
+                                    nn.Conv2d(30, 25, kernel_size=9, stride=1, padding=4),
+                                    nn.ReLU(),
+                                    nn.MaxPool2d(kernel_size=(2, 1)),
+                                    nn.Dropout(0.3),
+                                    nn.Conv2d(25, 20, kernel_size=7, stride=1, padding=3),
+                                    nn.ReLU(),
+                                    nn.MaxPool2d(kernel_size=(1, 2)),
+                                    nn.Conv2d(20, 15, kernel_size=5, stride=1, padding=2),
+                                    nn.ReLU(),
+                                    nn.Dropout(0.3),
+                                    nn.Conv2d(15, 10, kernel_size=3, stride=1, padding=1),
+                                    nn.ReLU(),
+                                    nn.Conv2d(10, 1, kernel_size=1, stride=1),
+                                    nn.ReLU(),
+                                    Flatten(),
+                                    nn.Linear(500, 256))
+
+    def forward(self, x):
+        return self.layers(x)
